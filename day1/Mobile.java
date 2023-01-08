543fa03967fbd261d31d158e4caa260f855e4417
VigneshMith <122201572+VigneshMith@users.noreply.github.com>
2023-01-08 14:23:08 +0530

    Add files via upload
    Day1

---
+++ day1/Mobile.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+    boolean isAndroid = false;
+    String brandname = "Apple";
+    System.out.println(brandname);
+    System.out.println(isAndroid);
+	}
+
+}
